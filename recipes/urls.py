d5a72ce56e0efa9d31a5bfec9498d4d7db7cf0f8
luccas <nascimentolucas2164@gmail.com>
2024-08-07 15:29:54 -0300

    feat ~ remove contato and sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,7 @@
-from django.contrib import admin
+
 from django.urls import path
-from django.http import HttpResponse
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request, "recipes/home.html", context={
         'name': "Luccas",
     })
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
