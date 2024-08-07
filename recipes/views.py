9e15396878394bf129b904bd99e6406365b0b630
luccas <nascimentolucas2164@gmail.com>
2024-08-07 15:26:29 -0300

    feat ~ add contato and sobre in def render

--- recipes/views.py
+++ recipes/views.py
@@ -7,7 +7,7 @@ def home(request):
     })
 
 def sobre(request):
-    return render(request, 'me-apague/temp.html')
+    return render(request, 'recipes/sobre.html')
 
 def contato(request):
-    return HttpResponse("CONTATO")
+    return render(request, 'recipes/contato.html')
