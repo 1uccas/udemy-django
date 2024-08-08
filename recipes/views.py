f2b191ab51a25e73a1ab35b7181a569568c36d57
luccas <nascimentolucas2164@gmail.com>
2024-08-08 17:31:34 -0300

    feat ~ A new URL for a new view

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,8 @@ def home(request):
     return render(request, "recipes/pages/home.html", context={
         'name': "Luccas",
     })
+    
+def recipe(request, id):
+    return render(request, "recipes/pages/home.html", context={
+        'name': "Luccas",
+    })
