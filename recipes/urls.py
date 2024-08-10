de0f5805835208536662fdc6202c9c59a155538e
luccas <nascimentolucas2164@gmail.com>
2024-08-10 20:50:09 -0300

    feat ~ Unique name in URLs and app_name for Namespace

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from recipes.views import home
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe), #<id> para receber o id na url | <int:> para receber apenas numero int
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"), #<id> para receber o id na url | <int:> para receber apenas numero int
 ]
