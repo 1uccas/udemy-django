10c19fd74f460c2157a7e1961fddc99145f46b0a
luccas <nascimentolucas2164@gmail.com>
2024-08-13 18:44:17 -0300

    feat ~ use get_object_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 from django.http import Http404
@@ -31,9 +31,11 @@ def category(request, category_id):
     })
     
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
+    """recipe = Recipe.objects.filter(
         id=id, is_published=True
-        ).order_by('-id').first()
+        ).order_by('-id').first()"""
+    
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
     
     return render(request, "recipes/pages/recipe-view.html", context={
         'recipe': recipe,
