ff9b150eb6d4591f46d4cc9927899098a1fb9bff
luccas <nascimentolucas2164@gmail.com>
2024-08-19 18:36:06 -0300

    test ~ 'No recipes found' when recipes do not exist

--- recipes/test/test_recipe_views.py
+++ recipes/test/test_recipe_views.py
@@ -24,3 +24,15 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
+        
+    def test_recipe_not_found_no_receipes_template(self):
+        response = self.client.get(reverse('recipes:home')) #peguei o conteudo da página home
+        self.assertIn(("<h1>No recipes found here 🥲</h1>").encode("utf-8"), response.content, "Error: HTML found")
+        
+        ''' 
+        Looking for my HTML Tag within the response content 
+        (for each string placed, it is necessary to encode to 'utf-8' to be transformed into n byte)
+        ! Or vice versa (response.content.decode(utf-8) -> byte to String)
+
+        '''
+        
