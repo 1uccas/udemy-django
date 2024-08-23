fca6f8dfb8856745e286a91e0a9f3185bb8fd7af
luccas <nascimentolucas2164@gmail.com>
2024-08-23 16:47:33 -0300

    test ~ check max_legth of filds in db

--- recipes/test/test_recipe_models_recipe.py
+++ recipes/test/test_recipe_models_recipe.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-
+from parameterized import parameterized
 from .test_recipes_base import TestRecipesBase
 
 class RecipeModelTest(TestRecipesBase):
@@ -9,6 +9,24 @@ class RecipeModelTest(TestRecipesBase):
     
     def test_recipe_title_raises_error_if_title_has_more_than_65_char(self):
         self.recipe.title = 'A' * 70
-        
+            
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+    
+    def test_recipe_fields_max_legth(self):
+        # testar o maximo de caracteres dos itens do banco de dados
+        filds = [
+                ('title', 65),
+                ('description', 165),
+                ('preparation_time_unit', 65),
+                ('servings_unit', 65),
+            ]
+        for fild, max_legth in filds:
+            # permite modificar ou adicionar atributos a objetos dinamicamente durante a execução do programa
+            setattr(self.recipe, fild, 'A' * (max_legth + 1))
+            
+            with self.assertRaises(ValidationError): #Try catch (chamada de exceção)
+                self.recipe.full_clean()
+            
+             
+        
