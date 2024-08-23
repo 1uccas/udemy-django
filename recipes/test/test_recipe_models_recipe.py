ae81d1d345f5eb7876654e80b389ca83d5e9efde
luccas <nascimentolucas2164@gmail.com>
2024-08-23 18:33:34 -0300

    test(refactor) ~ refactor commit [fca6f8d]
    Creating parameterized tests with parameterized

--- recipes/test/test_recipe_models_recipe.py
+++ recipes/test/test_recipe_models_recipe.py
@@ -7,27 +7,16 @@ class RecipeModelTest(TestRecipesBase):
         self.recipe = self.make_recipe()
         return super().setUp()
     
-    def test_recipe_title_raises_error_if_title_has_more_than_65_char(self):
-        self.recipe.title = 'A' * 70
-            
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+        breakpoint()
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
-    
-    def test_recipe_fields_max_legth(self):
-        # testar o maximo de caracteres dos itens do banco de dados
-        filds = [
-                ('title', 65),
-                ('description', 165),
-                ('preparation_time_unit', 65),
-                ('servings_unit', 65),
-            ]
-        for fild, max_legth in filds:
-            with self.subTest(fild=fild, max_legth=max_legth):
-                # permite modificar ou adicionar atributos a objetos dinamicamente durante a execução do programa
-                setattr(self.recipe, fild, 'A' * (max_legth + 0))
-                    
-                with self.assertRaises(ValidationError): #Try catch (chamada de exceção)
-                    self.recipe.full_clean()
-            
-             
+        
         
