2ba2f7239dae65d1206935652d249f677ea1ae08
luccas <nascimentolucas2164@gmail.com>
2024-08-28 16:43:24 -0300

    test ~ tell the view to return the correct template with TDD

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,7 +7,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('recipes/search/', views.search, name="search"),
     path('recipes/category/<int:category_id>/', views.category, name="category"), #<id> para receber o id na url | <int:> para receber apenas numero int
     path('recipes/<int:id>/', views.recipe, name="recipe"), #<id> para receber o id na url | <int:> para receber apenas numero int
-    path('recipes/search/', lambda request: ..., name="search")
 ]
