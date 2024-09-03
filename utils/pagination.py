ec541e8118249e9c2c85b55b67a4e7af6d76b1cd
luccas <nascimentolucas2164@gmail.com>
2024-09-03 18:04:04 -0300

    test ~ Adjusting the final range

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,4 +1,5 @@
 import math
+from pdb import set_trace
 
 def make_pagination(
     page_range, # interseção de páginas
@@ -9,12 +10,39 @@ def make_pagination(
     middle = math.ceil(qtd_page / 2)
     start = current_page - middle
     end = current_page + middle
+    total_pages = len(page_range)
     
+    # caso o start coloque como positivo caso ele for menor que zero (negativo) ou caso não for
     negative_start = abs(start) if start < 0 else 0
 
-    # caso o retorno do start se torne negativo
+    # caso o retorno do start se torne negativo, torne o start como zero e incremente ao negative_start
+    # end = end + negative_start
+   
     if start < 0:
         start = 0
         end += negative_start
-        
-    return page_range[start:end]
+    
+    #set_trace()
+    # current_page = 18 --> [17, 18, 19, 20]
+    # current_page = 20 --> [17, 18, 19, 20]
+    
+    # se o end for maior ou igual ao total de páginas, realize o seguinte calculo com o start:
+    # start recebe start menos (caso for negativo, deixe-o positivo) o calculo de: end menos o total de páginas
+    # isso para a interseção se tornar estatica quando o end for maior que o total de páginas
+    
+    if end >= total_pages:
+        start = start - abs(end - total_pages)   
+    
+    pagination = page_range[start:end]
+    
+    return {
+        'pagination': pagination,
+        'page_range': page_range,
+        'qtd_page': qtd_page,
+        'current_page': current_page,
+        'total_pages': total_pages,
+        'start_range': start,
+        'end_range': end,
+        'first_page_out_of_range': current_page > middle,
+        'last_page_out_of_range': end < total_pages,
+    }
--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -8,7 +8,7 @@ class PaginationTest(TestCase):
             page_range=list(range(1,21)),
             qtd_page=4,
             current_page=1
-            )
+            )['pagination']
         
         self.assertEqual([1,2,3,4], pagination)
         
@@ -17,7 +17,7 @@ class PaginationTest(TestCase):
             page_range=list(range(1,21)),
             qtd_page=4,
             current_page=2
-            )
+            )['pagination']
         
         self.assertEqual([1,2,3,4], pagination)
         
@@ -25,7 +25,7 @@ class PaginationTest(TestCase):
             page_range=list(range(1,21)),
             qtd_page=4,
             current_page=3
-            )
+            )['pagination']
         
         self.assertEqual([2,3,4,5], pagination)
         
@@ -33,6 +33,48 @@ class PaginationTest(TestCase):
             page_range=list(range(1,21)),
             qtd_page=4,
             current_page=4
-            )
+            )['pagination']
         
         self.assertEqual([3,4,5,6], pagination)
+    
+    def test_make_sure_middle_ranges_are_correct(self):
+        pagination=make_pagination(
+            page_range=list(range(1,21)),
+            qtd_page=4,
+            current_page=12
+            )['pagination']
+        
+        self.assertEqual([11,12,13,14], pagination)
+        
+        pagination=make_pagination(
+            page_range=list(range(1,21)),
+            qtd_page=4,
+            current_page=15
+            )['pagination']
+        
+        self.assertEqual([14,15,16,17], pagination)
+        
+        pagination=make_pagination(
+            page_range=list(range(1,21)),
+            qtd_page=4,
+            current_page=18
+            )['pagination']
+        
+        self.assertEqual([17,18,19,20], pagination)
+        
+        pagination=make_pagination(
+            page_range=list(range(1,21)),
+            qtd_page=4,
+            current_page=19
+            )['pagination']
+        
+        self.assertEqual([17,18,19,20], pagination)
+        
+        pagination=make_pagination(
+            page_range=list(range(1,21)),
+            qtd_page=4,
+            current_page=20
+            )['pagination']
+        
+        self.assertEqual([17,18,19,20], pagination)
+        
